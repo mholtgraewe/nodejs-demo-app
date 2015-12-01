5c8926f31ad7e9e9ba940c38675d3fb83e397db7
mholtgraewe <ogotay@gmail.com>
2015-12-02 00:17:49 +0100

    Fixed a copy&paste error (renamed $regForm to $loginForm)

--- public/test/login.js
+++ public/test/login.js
@@ -4,38 +4,38 @@
     var should = chai.should();
 
     describe('Login page', function () {
-        var $regForm = $('form#login-form');
+        var $loginForm = $('form#login-form');
 
         it('should have a login form', function () {
-            $regForm.should.exist;
+            $loginForm.should.exist;
         });
 
         describe('Login form', function () {
             it('should submit form data via POST', function () {
-                $regForm.should.have.attr('method', 'POST');
+                $loginForm.should.have.attr('method', 'POST');
             });
 
             it('should submit form data to the login endpoint', function () {
-                $regForm.should.have.attr('action', '/login');
+                $loginForm.should.have.attr('action', '/login');
             });
 
             it('should have a username field', function () {
-                $('input[name="username"]', $regForm)
+                $('input[name="username"]', $loginForm)
                         .should.exist.and.have.attr('type', 'text');
             });
 
             it('should have a password field', function () {
-                $('input[name="password"]', $regForm)
+                $('input[name="password"]', $loginForm)
                         .should.exist.and.have.attr('type', 'password');
             });
 
             it('should have a submit button', function () {
-                $('button:not([type]), button[type="submit"], input[type="submit"]', $regForm)
+                $('button:not([type]), button[type="submit"], input[type="submit"]', $loginForm)
                         .should.exist;
             });
 
             it('should feature a link to the registration page', function () {
-                $('a[href="/register"]', $regForm).should.exist;
+                $('a[href="/register"]', $loginForm).should.exist;
             });
         });
     });
